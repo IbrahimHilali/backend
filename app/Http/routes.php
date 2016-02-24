f02e56b0ed2613ac5fe16c9e26411eab0a69372e
Freeeed <friedrich@ueberreiter.com>
2016-02-25 00:53:52 +0100

    init

---
+++ app/Http/routes.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => 'web'], function () {
+
+    // Authentication Routes...
+    $this->get('login', 'Auth\AuthController@showLoginForm');
+    $this->post('login', 'Auth\AuthController@login');
+    $this->get('logout', 'Auth\AuthController@logout');
+
+    // Password Reset Routes...
+    $this->get('password/reset/{token?}', 'Auth\PasswordController@showResetForm');
+    $this->post('password/email', 'Auth\PasswordController@sendResetLinkEmail');
+    $this->post('password/reset', 'Auth\PasswordController@reset');
+
+    Route::group(['middleware' => 'auth'], function () {
+        Route::get('/', function () {
+            return redirect('home');
+        });
+
+        Route::get('/home', 'HomeController@index');
+
+        Route::resource('books', 'BooksController');
+        Route::resource('persons', 'PersonsController');
+    });
+});
---
+++ resources/views/persons/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+
+                <h1>Personendatenbank</h1>
+
+                {{ $persons->links() }}
+
+                <table class="table table-responsive">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nachname</th>
+                        <th>Vorname</th>
+                        <th><span class="glyphicon glyphicon-asterisk"></span></th>
+                        <th>&#10013;</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($persons->items() as $person)
+                        <tr>
+                            <td>{{ $person->id }}</td>
+                            <td>{{ $person->last_name }}</td>
+                            <td>{{ $person->first_name }}</td>
+                            <td>{{ $person->birth_date->format('d.m.Y') }}</td>
+                            <td>{{ $person->death_date->format('d.m.Y') }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+
+                {{ $persons->links() }}
+            </div>
+        </div>
+    </div>
+@endsection
