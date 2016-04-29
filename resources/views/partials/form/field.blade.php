7d01ed7d1a3eebbcccba8eb9c934bfd1d1146c41
David Bohn <davbohn@googlemail.com>
2016-04-30 01:29:24 +0200

    Added missing label for

--- resources/views/partials/form/field.blade.php
+++ resources/views/partials/form/field.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group{{ $errors->has($field) ? ' has-error' : '' }}">
-    <label class="col-sm-2 control-label">{{ field_name($field, $model) }}</label>
+    <label class="col-sm-2 control-label" for="input{{ ucfirst($field) }}">{{ field_name($field, $model) }}</label>
     <div class="col-sm-10">
-        <input class="form-control" name="{{ $field }}"
+        <input class="form-control" id="input{{ ucfirst($field) }}" name="{{ $field }}"
                value="{{ old($field, (isset($model) && !is_string($model)) ? $model->{$field}: null) }}">
         @if ($errors->has($field))
             <span class="help-block">
